8f7a79dfabada0ba9d98494d14b52b7fb4d01cd8
elasadel <elzbieta.sadel@gmail.com>
2026-03-09 09:08:36 +0100

    fix: wrap sensor count in NULLIF to avoid division by zero

--- grafana/queries/unhealthy_sensors_card.sql
+++ grafana/queries/unhealthy_sensors_card.sql
@@ -1,5 +1,5 @@
 SELECT 
   COUNT(DISTINCT CASE WHEN t.tags NOT LIKE '%maintenance%' THEN s.sensor END) * 100.00 
-  / COUNT(DISTINCT s.sensor) AS unhealthy_pct
+  / NULLIF(COUNT(DISTINCT s.sensor), 0) AS unhealthy_pct
 FROM sensors s
 LEFT JOIN sno_tasks t ON s.sensor = t.sensor AND t.status != 'done'
